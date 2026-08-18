a6d189d06c0e6246182a74dfd8fcc97f4c038bb0
Deepan <you@example.com>
2026-08-18 22:05:36 +0800

    Static preview

---
+++ _next/static/chunks/app/[locale]/rfq/page-0fdb5059fb9ee5db.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6189],{1946:(e,s,n)=>{Promise.resolve().then(n.bind(n,9941)),Promise.resolve().then(n.bind(n,3809))}},e=>{e.O(0,[3809,9801,9941,6708,4478,7358],()=>e(e.s=1946)),_N_E=e.O()}]);
